5409f541fee2b7d095ca2a9f7d38826b21ba4739
Thomas Brand <tom@trellis.ch>
2016-06-25 23:04:06 +0200

    allow custom .properties file to use, argument on command line

--- src/WebServer.java
+++ src/WebServer.java
@@ -51,6 +51,18 @@ public class WebServer
 //========================================================================
 	public WebServer(String[] args)
 	{
+		if(args.length>0 && (args[0].equals("-h") || args[0].equals("--help")))
+		{
+			System.out.println("WebServer Help");
+			System.out.println("Arguments: (properties file to use)");
+			System.out.println("If no argument given, default file '"+propertiesFileUri+"' will be used.");
+			System.exit(0);
+		}
+		else if(args.length>0)
+		{
+			propertiesFileUri=args[0];
+		}
+
 		if(!loadProps(propertiesFileUri))
 		{
 			System.err.println("could not load properties");
